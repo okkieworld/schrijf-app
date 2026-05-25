68121893484c078ea2dfeaf496c5c4cf048e070b
Okkieworld <okkieworld@hotmail.com>
2026-05-25 14:49:34 +0100

    d

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,13 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
+  // Dit zorgt ervoor dat de plugin niet vecht met app/manifest.ts:
+  register: true,
+  skipWaiting: true,
 });
 
 const nextConfig: NextConfig = {
-  // Dit zorgt ervoor dat als er gezocht wordt naar manifest.json, 
-  // Next.js de nieuwe foutloze manifest-route serveert!
-  async rewrites() {
-    return [
-      {
-        source: "/manifest.json",
-        destination: "/manifest.webmanifest",
-      },
-    ];
-  },
+  /* Jouw eventuele andere config opties */
 };
 
 export default withPWA(nextConfig);
